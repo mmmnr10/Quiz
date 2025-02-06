b318b9bba87d26187df1754a27fceb6ffa97eaad
RevanToma <tamrevan@gmail.com>
2025-02-06 14:52:37 +0100

    added laoding page

---
+++ quiz-app/pages/loading.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Loader from '@/styles/loader.gif';
+import Image from 'next/image';
+
+const LoadingPage = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <Image src={Loader} alt='Loading...' width={150} height={150} />
+    </div>
+  );
+};
+
+export default LoadingPage;
