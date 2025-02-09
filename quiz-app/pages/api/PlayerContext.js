25259133e4261503243b75ef884594f8a13a5881
mmmmm <mustaf.abdulahi@chasacademy.se>
2025-02-09 18:07:11 +0100

    start

--- quiz-app/pages/api/PlayerContext.js
+++ quiz-app/pages/api/PlayerContext.js
@@ -11,23 +11,19 @@ export const PlayerProvider = ({ children }) => {
   const [playerName, setPlayerName] = useState("");
   const [highScores, setHighScores] = useState([]);
 
-  // Hämta high scores från localStorage när komponenten laddas
   useEffect(() => {
     const savedHighScores =
       JSON.parse(localStorage.getItem("highScores")) || [];
     setHighScores(savedHighScores);
   }, []);
 
-  // Lägg till ett nytt high score
   const addHighScore = (name, score) => {
     const newHighScore = { name, score };
 
-    // Lägg till det nya high score i listan och sortera den
     const updatedHighScores = [...highScores, newHighScore].sort(
       (a, b) => b.score - a.score
     );
 
-    // Uppdatera tillståndet och localStorage
     setHighScores(updatedHighScores);
     localStorage.setItem("highScores", JSON.stringify(updatedHighScores));
   };
--- quiz-app/pages/api/startpage.js
+++ quiz-app/pages/api/startpage.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 const StartPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-800 text-white">
-      <h1 className="text-4xl font-bold mb-6">Välkommen till Quizet!</h1>
-      <Link href="/quiz">
+      <h1 className="text-4xl font-bold mb-6">Välkommen till Trivia Quizet!</h1>
+      <Link href="/triviaQuiz">
         <a className="bg-blue-500 text-white py-3 px-8 rounded-lg hover:bg-blue-400 transition">
-          Starta Quizet
+          Starta Trivia Quizet
         </a>
       </Link>
     </div>
