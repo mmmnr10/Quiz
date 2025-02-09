3726455f6b3c65ac9f4e3b4c89c41d40e17a9aa4
mmmmm <mustaf.abdulahi@chasacademy.se>
2025-02-09 15:17:30 +0100

    hh

--- quiz-app/pages/_app.js
+++ quiz-app/pages/_app.js
@@ -1,18 +1,22 @@
 import "@/styles/globals.css";
 import { TriviaProvider } from "@/context/QuizContext";
 import { ThemeProvider } from "next-themes";
+import { PlayerProvider } from "@/context/PlayerContext"; // Importera PlayerProvider
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider 
-    attribute={'class'}
-    defaultTheme="dark"
-    enableSystem
-    disableTransitionOnChange  
-    >  
-  <TriviaProvider>
-    <Component {...pageProps} />
-  </TriviaProvider>
-  </ThemeProvider>
-    )
+    <ThemeProvider
+      attribute={"class"}
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {/* PlayerProvider omsluter TriviaProvider för att dela spelardata */}
+      <PlayerProvider>
+        <TriviaProvider>
+          <Component {...pageProps} />
+        </TriviaProvider>
+      </PlayerProvider>
+    </ThemeProvider>
+  );
 }
